2d7db88b02842db038e6a587bb1f43f71d2d42ed
Dmitry Kolomeets <bde@ukr.net>
2022-07-20 21:15:40 +0300

    Initial commit

---
+++ src/main/java/chapter_1/BeerSong.java
@@ -0,0 +1,30 @@
+package chapter_1;
+
+public class BeerSong {
+
+    public static void main(String[] args) {
+
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+
+        while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "бутылка"; // one bottle
+            }
+
+            System.out.println(beerNum + " " + word + " пива на стене");
+            System.out.println(beerNum + " " + word + " пива");
+            System.out.println("Возьми одну.");
+            System.out.println("Пусти по кругу.");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " пива на стене");
+            } else {
+                System.out.println("Нет бутылок на стене");
+            } // End of Else
+        } // End of While
+    } // End of Methd Main
+} // End of Class
+
+
