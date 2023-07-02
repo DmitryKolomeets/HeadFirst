eabb46c90c18680d30aa96ccb786403fc426e9f1
Dmitry Kolomeets <bde@ukr.net>
2023-07-02 11:40:21 +0200

    New branch

--- src/main/java/Nail_Lambda/Test_1.java
+++ src/main/java/Nail_Lambda/Test_1.java
@@ -1,37 +1,40 @@
 package Nail_Lambda;
 
 
-
-
-interface Executable{
-    void execute();
+interface Executable {
+    int execute();
 }
 
-class Runner  {
-    public void run (Executable e) {
+class Runner {
+    public void run(Executable e) {
+        e.execute();
     }
 }
 
-class ExecutableImplmentation implements Executable{
-    @Override
-    public void execute() {
-        System.out.println("Hello");
-    }
-}
 
 
 public class Test_1 {
+
     public static void main(String[] args) {
 
         Runner runner = new Runner();
-        runner.run(new ExecutableImplmentation());
-runner.run(new Executable() {
-    @Override
-    public void execute() {
-        System.out.println("");
-    }
-});
 
+        runner.run(new Executable() {
+            @Override
+            public int execute() {
+                System.out.println("Hello");
+                System.out.println("Goodbuy!");
 
+                return 1;
+            }
+        });
+
+        runner.run(()-> {
+            System.out.println("Hello");
+            System.out.println("Goodbuy!");
+
+            return 1;
+
+        });
     }
 }
